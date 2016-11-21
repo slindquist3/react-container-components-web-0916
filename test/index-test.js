71aac515f70f53b3ea8b91756586cc6d83cce5c5
slindquist3 <slindquist3@gmail.com>
2016-11-21 13:42:34 -0500

    Done.

---
+++ test/index-test.js
@@ -0,0 +1,17 @@
+const React = require('react');
+
+
+describe('true_test', function () {
+  it('returns true', function () {
+    true
+  })
+})
+
+
+
+// describe('<DigitalClicker />', function () {
+//   it('should have the correct initial state', function () {
+//     const wrapper = shallow(<DigitalClicker />);
+//     expect(wrapper.state().timesClicked).toNotEqual(undefined, 'The `timesClicked` property does not exist in the state');
+//     expect(wrapper.state().timesClicked).toBeA('number').toEqual(0, 'The `timesClicked` property does have the right value');
+//   });
